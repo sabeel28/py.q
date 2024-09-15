3b6a22cd1deb54ef0e892dfb661aae4aaaa039be
Sabeel Aijaz <141610619+sabeel28@users.noreply.github.com>
2024-09-16 00:11:22 +0530

    Create IfElse.py

---
+++ Practice/IfElse.py
@@ -0,0 +1,21 @@
+marks=int(input("Enter the marks of student: "))
+
+if(marks>=90):
+    grade="A"
+elif(marks>=80 and marks<90):
+    grade="B"
+elif(marks>=70 and marks<80):
+    grade="C"
+else:
+    grade="D"
+print("grade of the student:",grade)
+
+# output
+# Enter the marks of student: 78
+# grade of the student: C
+--------------------------
+# age=int(input())
+# if(age>=18):
+#     print("Can vote")
+# else:
+#     print("Cannot vote")
